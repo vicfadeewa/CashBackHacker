88db045d500577689f26c85477ed9c761121cfaa
Victoriya Fadeeva <vic.f4deewa@gmail.com>
2025-11-04 12:42:04 +0300

    Update CashbackHackService logic from junit4 to testng

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -4,7 +4,6 @@ public class CashbackHackService {
     private final int boundary = 1000;
 
     public int remain(int amount) {
-        int remainder = amount % boundary;
-        return remainder == 0 ? 0 : boundary - remainder;
+        return boundary - amount % boundary;
     }
 }
